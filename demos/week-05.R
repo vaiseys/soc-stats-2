6dc72431ee902adc2a5330485443c63856472a4e
Stephen Vaisey <vaiseys@gmail.com>
2024-02-06 12:33:00 -0500

    updated week 5 demo

--- demos/week-05.R
+++ demos/week-05.R
@@ -32,10 +32,10 @@ qp1 <- glm(realrinc ~ college * (parcol + female + age + I(age^2)),
            family = "quasipoisson")
 mytidy(qp1) # spend time interpreting this!!!
 
-# alternative (negative binomial vs. Poisson)
-nb1 <- MASS::glm.nb(realrinc ~ college * (parcol + female + age + I(age^2)),
-                    data = d)
-mytidy(nb1)
+# # alternative (negative binomial vs. Poisson) [discussed but not used]
+# nb1 <- MASS::glm.nb(realrinc ~ college * (parcol + female + age + I(age^2)),
+#                     data = d)
+# mytidy(nb1)
 
 # ATE estimate
 avg_slopes(qp1,
@@ -43,6 +43,47 @@ avg_slopes(qp1,
            type = "response") |> 
   tidy()
 
+# ATE estimate rowwise
+mfx <- slopes(qp1,
+              variables = "college",
+              type = "response")
+View(select(mfx, estimate, female, parcol, age))
+
+# DIGRESSION: why are these numbers different for different people?
+## linear model with no interactions 
+## effects are the same because ADDITIVE LINK and NO INTERACTIONS
+lm1 <- lm(realrinc ~ college + parcol + female + age + I(age^2), 
+          data = d)
+mytidy(lm1)
+
+mfx_lm1 <- slopes(lm1,
+                  variables = "college")
+View(select(mfx_lm1, estimate, female, parcol, age))
+
+## linear model with interactions
+## effects differ because of INTERACTIONS
+lm2 <- lm(realrinc ~ college * (parcol + female + age + I(age^2)), 
+          data = d)
+mytidy(lm2)
+
+mfx_lm2 <- slopes(lm2,
+                  variables = "college")
+View(select(mfx_lm2, estimate, female, parcol, age))
+
+## log-link model with no interactions
+## effect differ because the log link is MULTIPLICATIVE
+p1 <- glm(realrinc ~ college + parcol + female + age + I(age^2), 
+          data = d,
+          family = "quasipoisson")
+mytidy(p1) # spend time interpreting this!!!
+
+mfx_p1 <- slopes(p1,
+                 variables = "college")
+View(select(mfx_p1, estimate, female, parcol, age))
+
+## the qp1 model has different estimates because of BOTH MULT. and INTERACTIONS
+
+## now back to the main story...
 # ATT/ATU estimate
 avg_slopes(qp1,
            variables = "college",
